5dd9230d60c173ae2d8926b4ad872426d6ab38c8
pbzweihander <pbzweihander@gmail.com>
2019-02-26 14:12:56 +0900

    fixed build script

--- gdua/build.rs
+++ gdua/build.rs
@@ -1,26 +1,30 @@
-use std::{env, fs::{File}, path::Path, io::{Write, Read}};
+use std::{
+    env,
+    fs::File,
+    io::{Read, Write},
+    path::Path,
+};
 
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
-    let ui_path = Path::new(&manifest_dir).join("../gdua-ui").canonicalize().unwrap();
-    let deploy_path = Path::new(&manifest_dir).join("../target/deploy").canonicalize().unwrap();
+    let out_path = Path::new(&out_dir);
+    let ui_path = Path::new(&manifest_dir)
+        .join("../gdua-ui")
+        .canonicalize()
+        .unwrap();
+    let deploy_path = Path::new(&manifest_dir)
+        .join("../target/deploy")
+        .canonicalize()
+        .unwrap();
 
     println!("cargo:rerun-if-changed={}", ui_path.display());
     println!("cargo:rerun-if-changed={}", deploy_path.display());
 
-    // let mut ui_build_command = Command::new("cargo");
-    // ui_build_command.args(&["web", "deploy", "-p", "gdua-ui"]);
-    // if !cfg!(debug_assertions) {
-    //     ui_build_command.arg("--release");
-    // }
-    // ui_build_command.status().unwrap();
-
-    let output_index_path = Path::new(&out_dir).join("index.html");
-    let index_path = Path::new(&manifest_dir).join("../target/deploy/index.html");
-    let css_path = Path::new(&manifest_dir).join("../target/deploy/styles.css");
-    let js_path = Path::new(&manifest_dir).join("../target/deploy/gdua-ui.js");
+    let index_path = deploy_path.join("index.html");
+    let css_path = deploy_path.join("styles.css");
+    let js_path = deploy_path.join("gdua-ui.js");
 
     let mut index_file = File::open(index_path).unwrap();
     let mut css_file = File::open(css_path).unwrap();
@@ -38,9 +42,16 @@ fn main() {
     drop(css_file);
     drop(js_file);
 
-    index = index.replace(r#"<link rel="stylesheet" href="styles.css" />"#, &inline_style(&css));
-    index = index.replace(r#"<script src="/gdua-ui.js"></script>"#, &inline_script(&js));
+    index = index.replace(
+        r#"<link rel="stylesheet" href="styles.css" />"#,
+        &inline_style(&css),
+    );
+    index = index.replace(
+        r#"<script src="/gdua-ui.js"></script>"#,
+        &inline_script(&js),
+    );
 
+    let output_index_path = out_path.join("index.html");
     let mut output_index_file = File::create(output_index_path).unwrap();
 
     output_index_file.write_all(index.as_bytes()).unwrap();
